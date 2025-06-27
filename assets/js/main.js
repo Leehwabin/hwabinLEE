80733957ef8156787e6848854fcfcb73f1e654c3
Leehwabin <hwabin2000@gmail.com>
2025-06-27 15:23:51 +0900

    up 0627

--- assets/js/main.js
+++ assets/js/main.js
@@ -185,3 +185,27 @@ function mobileNavToogle() {
 if (mobileNavToggleBtn) {
   mobileNavToggleBtn.addEventListener('click', mobileNavToogle);
 }
+
+  /**
+   * Hide mobile nav on same-page/hash links
+   */
+  document.querySelectorAll('#navmenu a').forEach(navmenu => {
+    navmenu.addEventListener('click', () => {
+      if (document.querySelector('.mobile-nav-active')) {
+        mobileNavToogle();
+      }
+    });
+
+  });
+
+  /**
+   * Toggle mobile nav dropdowns
+   */
+  document.querySelectorAll('.navmenu .toggle-dropdown').forEach(navmenu => {
+    navmenu.addEventListener('click', function(e) {
+      e.preventDefault();
+      this.parentNode.classList.toggle('active');
+      this.parentNode.nextElementSibling.classList.toggle('dropdown-active');
+      e.stopImmediatePropagation();
+    });
+  });
